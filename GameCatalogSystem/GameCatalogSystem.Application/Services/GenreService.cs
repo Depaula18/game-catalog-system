61c82dd2f92e5286ef01173f573dbb9572841503
Depaula18 <depaula.murilo13@gmail.com>
2026-04-16 18:31:16 -0300

    feat: implementa camada application e api para a entidade genre

---
+++ GameCatalogSystem/GameCatalogSystem.Application/Services/GenreService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameCatalogSystem.Application.DTOs.Genre;
+using GameCatalogSystem.Application.Services.Interfaces;
+using GameCatalogSystem.Domain.Entities;
+using GameCatalogSystem.Domain.Repositories;
+
+namespace GameCatalogSystem.Application.Services;
+public class GenreService : IGenreService
+{
+    private readonly IGenreRepository _genreRepository;
+    public GenreService(IGenreRepository genreRepository)
+    {
+        _genreRepository = genreRepository;
+    }
+    public async Task<IEnumerable<GenreResponseDTO>> GetAllAsync()
+    {
+        var genres = await _genreRepository.GetAllAsync();
+        return genres.Select(g => new GenreResponseDTO
+        {
+            Id = g.Id,
+            Name = g.Name,
+            Description = g.Description
+        });
+    }
+
+    public async Task<GenreResponseDTO?> GetByIdAsync(Guid id)
+    {
+        var genre = await _genreRepository.GetByIdAsync(id);
+        if (genre == null) return null;
+        return new GenreResponseDTO
+        {
+            Id = genre.Id,
+            Name = genre.Name,
+            Description = genre.Description
+        };
+    }
+
+    public async Task<GenreResponseDTO> CreateAsync(CreateGenreRequestDTO dto)
+    {
+        var genre = new Genre(dto.Name, dto.Description);
+
+        await _genreRepository.AddAsync(genre);
+        await _genreRepository.SaveChangesAsync();
+
+        return new GenreResponseDTO
+        {
+            Id = genre.Id,
+            Name = genre.Name,
+            Description = genre.Description
+        };
+    }
+}
---
+++ GameCatalogSystem/GameCatalogSystem.Application/Services/Interfaces/IGenreService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameCatalogSystem.Application.DTOs.Genre;
+
+namespace GameCatalogSystem.Application.Services.Interfaces;
+
+public interface IGenreService
+{
+    Task<IEnumerable<GenreResponseDTO>> GetAllAsync();
+    Task<GenreResponseDTO?> GetByIdAsync(Guid id);
+    Task<GenreResponseDTO> CreateAsync(CreateGenreRequestDTO request);
+}
+
