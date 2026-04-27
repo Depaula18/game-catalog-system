a93804f0f878f4cbb3727c2588bd2d12f2589d05
Depaula18 <depaula.murilo13@gmail.com>
2026-04-27 16:00:07 -0300

    suporte ao Postgres e o uso de EnvironmentVariables

--- GameCatalogSystem/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ GameCatalogSystem/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1572bb55abd53e51f8be38d2e5b3c953850d39c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65b3bf650262084e57c3c45828c9ccc3db58e038")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
