1542398f9be546194561df67c8b0e22f9bd3ab96
Depaula18 <depaula.murilo13@gmail.com>
2026-04-27 17:17:01 -0300

    config: aponta api para a url de producao no render

--- frontend-react/src/services/api.ts
+++ frontend-react/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://localhost:7277/api',
+  //baseURL: 'https://localhost:7277/api',
+  baseURL: 'https://game-catalog-api-xk1h.onrender.com'
 });
 
 api.interceptors.request.use(
